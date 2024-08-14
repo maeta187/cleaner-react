9743552fa4d6cd416470f2e6cacaee2ba3f441ac
maeta187 <nexus18789@gmail.com>
2024-08-14 14:36:03 +0900

    ✨ : componentまとめ

---
+++ src/app/improvement/component/Dialog.tsx
@@ -0,0 +1,22 @@
+import type { ReactNode } from "react";
+
+interface DialogProps {
+	children: ReactNode;
+	header: string;
+	open: boolean;
+	onClose: () => void;
+}
+
+export const Dialog = ({ children, header, open, onClose }: DialogProps) => {
+	return (
+		<dialog open={open}>
+			<div className="iconWrapper">
+				<button type="button" onClick={onClose}>
+					✖️
+				</button>
+			</div>
+			<h2 className="dialogTitle">{header}</h2>
+			{children}
+		</dialog>
+	);
+};
---
+++ src/app/improvement/component/page.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+import { ImprovementTodo } from "./improvement-Todo";
+
+const Page: NextPage = () => {
+	return (
+		<div>
+			<h1>Improvement-component</h1>
+			<ImprovementTodo />
+		</div>
+	);
+};
+
+export default Page;
