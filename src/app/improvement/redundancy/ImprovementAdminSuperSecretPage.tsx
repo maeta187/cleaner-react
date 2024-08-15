494c1fa9cdee0f5b690bfe91986ecc8bacdf8c15
maeta187 <nexus18789@gmail.com>
2024-08-15 18:56:38 +0900

    ✨ : redundancy-useEffectのまとめ

---
+++ src/app/improvement/redundancy/ImprovementAdminSuperSecretPage.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import { use, useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+
+const mockAdmin = () => {
+  return {
+    isAdmin: true
+  }
+}
+
+export const ImprovementAdminSuperSecretPage = () => {
+  const { isAdmin } = mockAdmin()
+  const router = useRouter()
+
+  // useEffectを使わないことで、チラつきを防ぐ
+  if (!isAdmin) {
+    return router.push('/')
+  }
+
+  return (
+    <div>
+      <h1>Admin Super Secret Page</h1>
+      <p>Admin Only</p>
+    </div>
+  )
+}
